768fb8b1e585cbe5cb88d7c6c287bd3f75ea9292
LinusPotocnik <linuspotocnik@icloud.com>
2022-06-30 11:25:26 +0200

    Remove async wrapper function

--- Bot.js
+++ Bot.js
@@ -11,22 +11,21 @@ const events = fs
 	.filter((file) => file.endsWith('.js'));
 
 // Create an asynchronus function and run it instantly
-(async () => {
-	// Check for an event and execute the corresponding file in ./events
-	for (let event of events) {
-		// The #events ES6 import-abbreviation is defined in the package.json
-		const eventFile = await import(`#events/${event}`);
-		// But first check if it's an event emitted once
-		if (eventFile.once)
-			client.once(eventFile.name, (...args) => {
-				eventFile.invoke(...args);
-			});
-		else
-			client.on(eventFile.name, (...args) => {
-				eventFile.invoke(...args);
-			});
-	}
-})();
+
+// Check for an event and execute the corresponding file in ./events
+for (let event of events) {
+	// The #events ES6 import-abbreviation is defined in the package.json
+	const eventFile = await import(`#events/${event}`);
+	// But first check if it's an event emitted once
+	if (eventFile.once)
+		client.once(eventFile.name, (...args) => {
+			eventFile.invoke(...args);
+		});
+	else
+		client.on(eventFile.name, (...args) => {
+			eventFile.invoke(...args);
+		});
+}
 
 // Login with the environment data
 client.login(process.env.BOT_TOKEN);
