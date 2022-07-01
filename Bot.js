9fe9ce092e77c8fda5d85d2ff7e05909b867d84c
LinusPotocnik <linuspotocnik@icloud.com>
2022-07-01 19:20:07 +0200

    General improvements

--- Bot.js
+++ Bot.js
@@ -10,11 +10,11 @@ const events = fs
 	.readdirSync('./events')
 	.filter((file) => file.endsWith('.js'));
 
-// Create an asynchronus function and run it instantly
-
 // Check for an event and execute the corresponding file in ./events
 for (let event of events) {
 	// The #events ES6 import-abbreviation is defined in the package.json
+	// Note that the entries in the list of files (created by readdirSync) end with .js,
+	// so the abbreviation is different to the #commands abbreviation
 	const eventFile = await import(`#events/${event}`);
 	// But first check if it's an event emitted once
 	if (eventFile.once)
